e26e08c8e0fb6e3f65373fbecfc4c74937cd1977
Piotr Lik <piotr.lik06@gmail.com>
2026-07-07 23:07:48 +0200

    Update window blur

--- hypr/hyprconfig/visuals.lua
+++ hypr/hyprconfig/visuals.lua
@@ -39,8 +39,8 @@ hl.config({
 
         blur = {
             enabled   = true,
-            size      = 3,
-            passes    = 1,
+            size      = 5,
+            passes    = 2,
             vibrancy  = 0.1696,
         },
     },

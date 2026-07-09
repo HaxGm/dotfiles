75c8be55b1e917adf5ccaf957dd0bdc2b2c91224
Piotr Lik <piotr.lik06@gmail.com>
2026-07-09 18:05:25 +0200

    Minor fixes

--- hypr/hyprconfig/autostart.lua
+++ hypr/hyprconfig/autostart.lua
@@ -5,9 +5,9 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 hl.on("hyprland.start", function () 
+  -- Pass Wayland environment variables to systemd user services
+  hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
   hl.exec_cmd("waybar")
   hl.exec_cmd("swaync")
   hl.exec_cmd("awww-daemon")
-  -- Pass Wayland environment variables to systemd user services
-  hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
  end)

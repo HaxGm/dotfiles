65bcb1f923f52f1a0d0dbb0190c14d6993ee3348
Piotr Lik <piotr.lik06@gmail.com>
2026-07-16 17:50:29 +0200

    Fix hyprlock not having screencopy permissions

--- hypr/hyprconfig/permissions.lua
+++ hypr/hyprconfig/permissions.lua
@@ -14,4 +14,5 @@ hl.config({
 
 hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
 hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
+hl.permission("/usr/(bin|local/bin)/hyprlock", "screencopy", "allow")
 -- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
